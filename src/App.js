d207ff8b903613a78f2ae433d01e38fef77abc14
camilalopes <kmilalopes10@gmail.com>
2019-01-21 00:21:01 -0200

    add the CreateContact component and use Routes to control the page content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 class App extends Component {
   state = {
@@ -20,7 +22,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+        <Route exact path='/' render={() => (
+          <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+        )}/>
+        <Route path='/create' component={CreateContact}/>
       </div>
     );
   }
